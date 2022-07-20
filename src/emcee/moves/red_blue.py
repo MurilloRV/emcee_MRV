88f10b724ebe32737ad64c3bc75b94e7f3dd6d62
MurilloRV <106568191+MurilloRV@users.noreply.github.com>
2022-07-21 00:12:48 +0200

    New_state now returned

--- src/emcee/moves/red_blue.py
+++ src/emcee/moves/red_blue.py
@@ -103,4 +103,4 @@ class RedBlueMove(Move):
             new_state = State(q, log_prob=new_log_probs, blobs=new_blobs)
             state = self.update(state, new_state, accepted, S1)
 
-        return state, accepted
+        return state, accepted, new_state
